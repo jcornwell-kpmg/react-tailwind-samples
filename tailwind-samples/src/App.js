32a5c6f2d140d4510fde453c124d3eb507e365f9
Jacob Cornwell <jcornwell@kpmg.com>
2022-03-25 10:10:01 -0400

    Adding button styling example

--- tailwind-samples/src/App.js
+++ tailwind-samples/src/App.js
@@ -21,12 +21,34 @@ function App() {
       </header>
       */}
 
+      {/* Example of adding Tailwind utility class directly to button elements.
       <section className="p-8 flex justify-between">
-        <button className="h-12 min-w-40 bg-blue-700 hover:bg-blue-500 text-white font-medium py-1 px-12 rounded-3xl border-3 border-blue-700">Primary</button>
+        <button className="h-12 min-w-40 bg-cust-blue hover:bg-cust-blue-lighter text-white font-medium py-1 px-12 rounded-3xl border-3 border-cust-blue">Primary</button>
+        
+        <button className="h-12 min-w-40 bg-white text-cust-blue font-medium hover:font-bold py-1 px-12 rounded-3xl border-3 hover:border-5 border-cust-blue">Secondary</button>
 
-        <button className="h-12 min-w-40 bg-white text-blue-700 font-medium hover:font-bold py-1 px-12 rounded-3xl border-3 hover:border-5 border-blue-700">Secondary</button>
+        <button className="h-12 min-w-40 bg-cust-gray-lighter hover:bg-cust-gray text-cust-blue font-medium py-1 px-12 rounded-3xl border-3 border-cust-gray-lighter hover:border-cust-gray">Tertiary</button>
+      </section>
 
-        <button className="h-12 min-w-40 bg-gray-100 hover:bg-gray-200 text-blue-700 font-medium py-1 px-12 rounded-3xl border-3 border-gray-100 hover:border-gray-200">Tertiary</button>
+      <section className="p-8 flex justify-between bg-neutral-900">
+        <button className="h-12 min-w-40 bg-white hover:bg-cust-zinc text-black font-medium py-1 px-12 rounded-3xl border-3 border-white hover:border-cust-zinc">Primary</button>
+
+        <button className="h-12 min-w-40 bg-black text-white font-medium hover:font-bold py-1 px-12 rounded-3xl border-3 hover:border-5 border-white">Secondary</button>
+
+        <button className="h-12 min-w-40 bg-cust-dark-gray hover:bg-cust-dark-gray-lighter text-white font-medium py-1 px-12 rounded-3xl border-3 border-cust-dark-gray hover:border-cust-dark-gray-lighter">Tertiary</button>
+      </section>
+      */}
+
+      <section className="p-8 flex flex-col md:flex-row flex-nowrap justify-stretch md:justify-around">
+        <button className="btn btn-primary m-2 md:m-0">Primary</button>
+        <button className="btn btn-secondary m-2 md:m-0">Secondary</button>
+        <button className="btn btn-tertiary m-2 md:m-0">Tertiary</button>
+      </section>
+
+      <section className="bg-neutral-900 p-8 flex flex-col md:flex-row flex-nowrap justify-stretch md:justify-around">
+        <button className="btn btn-primary-dark m-2 md:m-0">Primary</button>
+        <button className="btn btn-secondary-dark m-2 md:m-0">Secondary</button>
+        <button className="btn btn-tertiary-dark m-2 md:m-0">Tertiary</button>
       </section>
     </div>
   );
--- tailwind-samples/tailwind.config.js
+++ tailwind-samples/tailwind.config.js
@@ -2,9 +2,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      'borderWidth': {
+      colors: {
+        'cust-blue': '#2e6191',
+        'cust-blue-lighter': '#2e6191B3',
+        'cust-gray': '#d4d3d3',
+        'cust-gray-lighter': '#e9e9e9',
+        'cust-dark-gray': '#8b8b8b',
+        'cust-dark-gray-lighter': '#a8a8a8',
+        'cust-zinc': '#faf9f9e6'
+      },
+      borderWidth: {
         '3': '3px',
         '5': '5px'
+      },
+      borderRadius: {
+        '2rem': '2rem'
       }
     },
   },
